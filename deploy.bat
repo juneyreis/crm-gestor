bf09aa1f10779e78029cfcdbe4259dacc6e001c8
Juney Reis <juneyreis@gmail.com>
2026-02-11 09:12:29 -0300

    Ajuste módulo de comissões

---
+++ deploy.bat
@@ -0,0 +1,27 @@
+@echo off
+:: Arquivo em lote para deploy nas plataformas.
+:: Verifica se uma mensagem foi passada como argumento
+if "%~1"=="" (
+    set /p msg="Digite a mensagem do commit: "
+) else (
+    set msg=%~1
+)
+
+echo === INICIANDO DEPLOY DUPLO (GITHUB + VERCEL) ===
+
+:: 1. Git Push (Código-fonte)
+echo --- Enviando codigo para o GitHub com a mensagem: "%msg%"
+git add .
+git commit -m "%msg%"
+git push origin master
+
+:: 2. Deploy GitHub Pages
+echo --- Fazendo deploy no GitHub Pages...
+npm run deploy:github
+
+:: 3. Deploy Vercel
+echo --- Fazendo deploy na Vercel...
+npm run deploy:vercel
+
+echo === TODOS OS DEPLOYS CONCLUIDOS! ===
+pause
